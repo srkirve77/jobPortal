ea0ff861b8f81b186b703f3cddbd6d6bb26634cb
Shubham kirve <srkirve77@gmail.com>
2020-10-04 19:28:13 +0530

    Updates

---
+++ precache-manifest.ab8265846403c0313fec2cafdea36134.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eaaa676f81edc4064add94b38367a259",
+    "url": "/jobPortal/index.html"
+  },
+  {
+    "revision": "a7a8efc1d9bb0a2b64e5",
+    "url": "/jobPortal/static/css/main.0986b984.chunk.css"
+  },
+  {
+    "revision": "606ef12c0c15d7e056f4",
+    "url": "/jobPortal/static/js/2.31028099.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/jobPortal/static/js/2.31028099.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7a8efc1d9bb0a2b64e5",
+    "url": "/jobPortal/static/js/main.9186fc60.chunk.js"
+  },
+  {
+    "revision": "5513ecb5c48d175390fc",
+    "url": "/jobPortal/static/js/runtime-main.b33b6129.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/jobPortal/static/media/logo.5d5d9eef.svg"
+  }
+]);
